394ae58117315e6cf00d60a9f9bd10264fa94da6
Dina-Atef12 <dinaatef684@gmail.com>
2023-01-27 00:17:03 +0200

    .AddBtn.Text = Add

--- manage/department.cs
+++ manage/department.cs
@@ -274,7 +274,7 @@ namespace EmployeeMgmt1
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject(" pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(45, 149);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(86, 60);
