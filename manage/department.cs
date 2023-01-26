7a39964eeb66d5badbac6c81682d84e98e6b671b
Dina-Atef12 <dinaatef684@gmail.com>
2023-01-27 00:41:44 +0200

    try

--- manage/department.cs
+++ manage/department.cs
@@ -28,7 +28,7 @@ namespace EmployeeMgmt1
 
         private void AddBtn_Click(object sender, EventArgs e)
         {
-            try
+            try 
             {
                 if (DepNameTb.Text == "")
                 {
