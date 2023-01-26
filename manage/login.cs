ea1b9868cfdcf3648a260ed01e4feab82f60303e
Dina-Atef12 <dinaatef684@gmail.com>
2023-01-26 23:02:39 +0200

    button1.Text = LOGIN

--- manage/login.cs
+++ manage/login.cs
@@ -119,7 +119,7 @@ namespace manage
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(102, 37);
             this.button1.TabIndex = 26;
-            this.button1.Text = "LOGIN";
+            this.button1.Text = "LOGIN"; 
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
